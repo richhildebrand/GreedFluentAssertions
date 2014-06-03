a3322e405d093c71599119ad1f6b5443b6730aae
Rich <rich.hildebrand@gmail.com>
2014-06-02 23:11:59 -0400

    basic ones and fives scoring

---
+++ GreedFluentAssertions/GreedFluentAssertions/Greed.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GreedFluentAssertions
+{
+    public class Greed
+    {
+        public int ScoreDiceRolls(List<int> diceRolls)
+        {
+            var score = 0;
+            score += ScoreOnes(diceRolls);
+            score += ScoreFives(diceRolls);
+
+            return score;
+        }
+  
+        private int ScoreFives(List<int> diceRolls)
+        {
+            var fives = diceRolls.Where(dr => dr == 5).Count();
+            return fives * 50;
+        }
+  
+        private int ScoreOnes(List<int> diceRolls)
+        {
+            var ones = diceRolls.Where(dr => dr == 1).Count();
+            return ones * 100;
+        }
+    }
+}
---
+++ GreedFluentAssertions/Tests/ScoreDiceRollsShould.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using GreedFluentAssertions;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class ScoreDiceRollsShould
+    {
+        private Greed _greed;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _greed = new Greed();
+        }
+
+        [Test]
+        public void Return100WhenGivenOneOne()
+        {
+            var diceRolls = new List<int>() { 1 };
+            var score = _greed.ScoreDiceRolls(diceRolls);
+
+            score.Should().Be(100);
+        }
+
+        [Test]
+        public void Return50WhenGivenOneFive()
+        {
+            var diceRolls = new List<int>() { 5 };
+            var score = _greed.ScoreDiceRolls(diceRolls);
+
+            score.Should().Be(50);
+        }
+
+    }
+}
