c7149e7e10d322fd195acbf5032bbaad7802dd2d
Rich <rich.hildebrand@gmail.com>
2014-06-02 23:28:41 -0400

    5 of 6 working

--- GreedFluentAssertions/GreedFluentAssertions/Greed.cs
+++ GreedFluentAssertions/GreedFluentAssertions/Greed.cs
@@ -10,21 +10,51 @@ namespace GreedFluentAssertions
         {
             var score = 0;
             score += ScoreOnes(diceRolls);
+            score += ScoreTwos(diceRolls);
+            score += ScoreThrees(diceRolls);
+            score += ScoreFours(diceRolls);
             score += ScoreFives(diceRolls);
 
             return score;
+        }
+
+        private int ScoreOnes(List<int> diceRolls)
+        {
+            var ones = diceRolls.Where(dr => dr == 1).Count();
+
+            int setsOfThree = ones / 3;
+            ones -= setsOfThree * 3;
+
+            return ones * 100 + setsOfThree * 1000;
+        }
+
+        private int ScoreTwos(List<int> diceRolls)
+        {
+            var twos = diceRolls.Where(dr => dr == 2).Count();
+            var setsOfThree = twos / 3;
+            return setsOfThree * 200;
+        }
+
+        private int ScoreThrees(List<int> diceRolls)
+        {
+            var threes = diceRolls.Where(dr => dr == 3).Count();
+            var setsOfThree = threes / 3;
+
+            return setsOfThree * 300;
+        }
+  
+        private int ScoreFours(List<int> diceRolls)
+        {
+            var fours = diceRolls.Where(dr => dr == 4).Count();
+            var setsOfThree = fours / 3;
+
+            return setsOfThree * 400;
         }
   
         private int ScoreFives(List<int> diceRolls)
         {
             var fives = diceRolls.Where(dr => dr == 5).Count();
             return fives * 50;
-        }
-  
-        private int ScoreOnes(List<int> diceRolls)
-        {
-            var ones = diceRolls.Where(dr => dr == 1).Count();
-            return ones * 100;
         }
     }
 }
--- GreedFluentAssertions/Tests/ScoreDiceRollsShould.cs
+++ GreedFluentAssertions/Tests/ScoreDiceRollsShould.cs
@@ -19,7 +19,7 @@ namespace Tests
         }
 
         [Test]
-        public void Return100WhenGivenOneOne()
+        public void Return100_WhenGivenOneOne()
         {
             var diceRolls = new List<int>() { 1 };
             var score = _greed.ScoreDiceRolls(diceRolls);
@@ -28,12 +28,75 @@ namespace Tests
         }
 
         [Test]
-        public void Return50WhenGivenOneFive()
+        public void Return50_WhenGivenOneFive()
         {
             var diceRolls = new List<int>() { 5 };
             var score = _greed.ScoreDiceRolls(diceRolls);
 
             score.Should().Be(50);
+        }
+
+        [Test]
+        public void Return1000_WhenGivenThreeOnes()
+        {
+            var diceRolls = new List<int>() { 1, 1, 1 };
+            var score = _greed.ScoreDiceRolls(diceRolls);
+
+            score.Should().Be(1000);
+        }
+
+        [Test]
+        public void Return1200_WhenGivenFiveOnes()
+        {
+            var diceRolls = new List<int>() { 1, 1, 1, 1, 1 };
+            var score = _greed.ScoreDiceRolls(diceRolls);
+
+            score.Should().Be(1200);
+        }
+
+        [Test]
+        public void Return2100_WhenGivenSevenOnes()
+        {
+            var diceRolls = new List<int>() { 1, 1, 1, 1, 1, 1, 1 };
+            var score = _greed.ScoreDiceRolls(diceRolls);
+
+            score.Should().Be(2100);
+        }
+
+        [Test]
+        public void ReturnZero_WhenGivenTwoTwos()
+        {
+            var diceRolls = new List<int> { 2, 2 };
+            var score = _greed.ScoreDiceRolls(diceRolls);
+
+            score.Should().Be(0);
+        }
+
+        [Test]
+        public void Return200_WhenGivenThreeTwos()
+        {
+            var diceRolls = new List<int> { 2, 2, 2 };
+            var score = _greed.ScoreDiceRolls(diceRolls);
+
+            score.Should().Be(200);
+        }
+
+        [Test]
+        public void Return300_WhenGivenFourThrees()
+        {
+            var diceRolls = new List<int> { 3, 3, 3 };
+            var score = _greed.ScoreDiceRolls(diceRolls);
+
+            score.Should().Be(300);
+        }
+
+        [Test]
+        public void Return800_WhenGivenSixFours()
+        {
+            var diceRolls = new List<int> { 4, 4, 4, 4, 4, 4 };
+            var score = _greed.ScoreDiceRolls(diceRolls);
+
+            score.Should().Be(800);
         }
 
     }
