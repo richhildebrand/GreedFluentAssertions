41340c59c11d9decb8400d9895c071a9ef5d55fd
Rich <rich.hildebrand@gmail.com>
2014-06-02 22:59:29 -0400

    added test project

---
+++ GreedFluentAssertions/GreedFluentAssertions/GreedPrinter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace GreedFluentAssertions
+{
+    public class GreedPrinter
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
--- GreedFluentAssertions/Tests/Class1.cs
+++ GreedFluentAssertions/Tests/Class1.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GreedFluentAssertions
+namespace Tests
 {
-    class Program
+    public class Class1
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
